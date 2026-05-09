39aa98f8606a53b9b86bfe5ba51af99c26da1eb1
bmanayagath <bijumanayagath@gmail.com>
2026-05-09 07:39:40 +0300

    Update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -36,7 +36,7 @@ class DefaultFirebaseOptions {
     messagingSenderId: '938320861231',
     projectId: 'investment-calculator-811fd',
     storageBucket: 'investment-calculator-811fd.firebasestorage.app',
-    iosBundleId: 'villa.management.app',
+    iosBundleId: 'com.example.villabooks',
   );
 
   static const FirebaseOptions macos = ios;
